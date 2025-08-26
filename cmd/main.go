db72a773f90a58ec4c6191e1c6fa268ba1fe1d88
Tomas Ozolinsius <tomas.ozolinsius@tglab.com>
2025-08-26 13:10:03 +0300

    feat: add Post basic functionality

--- cmd/main.go
+++ cmd/main.go
@@ -3,7 +3,9 @@ package main
 import (
 
 	"news-feed/internal/user"
+	"news-feed/internal/post"
 	"news-feed/internal/db"
+	"news-feed/internal/auth"
 	"github.com/gin-gonic/gin"
 
 	"log"
@@ -20,6 +22,15 @@ func main() {
 	
 	router.POST("/register", userHandler.RegisterUser)
 	router.POST("/login", userHandler.LoginUser)
+
+	postRepo := post.NewPostRepository(dbConn)
+	postService := post.NewPostService(postRepo)
+	postHandler := post.NewPostHandler(postService)
+
+	router.GET("/post/:id", auth.AuthMiddleware(), postHandler.GetPostByID)
+	//Temporary for receiving all posts
+	router.GET("/posts", auth.AuthMiddleware(), postHandler.GetPosts)
+	router.POST("/posts", auth.AuthMiddleware(), postHandler.CreatePost)
 
 	log.Println("Lets Start Application")
 	router.Run(":8080")
